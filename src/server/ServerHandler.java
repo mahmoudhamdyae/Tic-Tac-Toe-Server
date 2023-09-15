1cb1b5a5439aecf7e423dcf08d603cfec908c3f2
khaledeid1k <khaled.eid1k1k@gmail.com>
2023-09-15 03:16:02 +0300

    add feature play with other online

--- src/server/ServerHandler.java
+++ src/server/ServerHandler.java
@@ -14,13 +14,17 @@ import server.models.User;
 import server.utils.Constants;
 
 public class ServerHandler extends Thread {
-    
-    DataInputStream dataInputStream ;
+
+    DataInputStream dataInputStream;
     PrintStream prrintStream;
+    String useNameG;
     Boolean isServerRunning;
-     DataAccess dataAccess = new DataAccess();
-            String typeOfOperation;
-                 BufferedReader bufferedReader;
+    DataAccess dataAccess = new DataAccess();
+    String typeOfOperation;
+    BufferedReader bufferedReader;
+    String result = "";
+    String userName = "";
+    String opUserName = "";
 
     public ServerHandler(Socket socket, Boolean isServerRunning) {
         this.isServerRunning = isServerRunning;
@@ -33,196 +37,113 @@ public class ServerHandler extends Thread {
         }
         start();
     }
-    
-    boolean IsServerRunn(){
-    
-    return false;
-    }
-    
- 
-    
+
     @Override
     public void run() {
-        while(true) {
+        while (true) {
             try {
-           bufferedReader = new BufferedReader(new InputStreamReader(dataInputStream));
-                 typeOfOperation = bufferedReader.readLine();
-
+                bufferedReader = new BufferedReader(new InputStreamReader(dataInputStream));
+                typeOfOperation = bufferedReader.readLine();
+                System.out.println(typeOfOperation);
                 if (isServerRunning) {
 
-                 prrintStream.println(Constants.SERVER_RUNNING); // Server Running
-             
-               
-                 
-                 if(typeOfOperation!=null){
+                    prrintStream.println(Constants.SERVER_RUNNING);
 
-                     if (typeOfOperation.equals(Constants.LOGIN)) { loginChecker();  }
-                
-                     else if (typeOfOperation.equals(Constants.REGISTER)) {RegisterChecker();}
-                
-                     else if (typeOfOperation.equals(Constants.GET_USERS)) {getUsers();} 
-     
-                
-                     
-                     else if (typeOfOperation.equals(Constants.SEBD_REQUEST_TO_PLAY)) {
-                       
-                        String userName = bufferedReader.readLine();
-                        String opUserName = bufferedReader.readLine();
-                           bufferedReader.readLine();
-                        if(isServerRunning){
-                                                prrintStream.println(Constants.SERVER_RUNNING);
-                                                bufferedReader.readLine();
-                                                bufferedReader.readLine();
-                                                
-                                                                               prrintStream.println(opUserName);
-                            prrintStream.println(userName); // Return UserName2
-                        prrintStream.println(Constants.SEBD_REQUEST_TO_PLAY); // Return PLAY_WITH_USER
-                            
-                        
-
-
-                        }else{
-                        
-                  prrintStream.println(Constants.SERVER_STOP);
+                    if (typeOfOperation != null) {
+                        if (typeOfOperation.equals(Constants.LOGIN)) {
+                            loginChecker();
+                        } else if (typeOfOperation.equals(Constants.REGISTER)) {
+                            RegisterChecker();
+                        } else if (typeOfOperation.equals(Constants.GET_USERS)) {
+                            getUsers();
+                        } else if (typeOfOperation.equals(Constants.SEND_REQUEST_TO_PLAY)) {
+                            playWithOther();
                         }
-
-                        
-                    } 
-                     
-                     
-                     
-                     
-                     
-                     
-                     
-                     
-                     else if (typeOfOperation.equals(Constants.PLAY_WITH_USER_RESPONSE)) {
-                        String userName = dataInputStream.readLine();
-                        String userResponse = dataInputStream.readLine();
-                    bufferedReader.readLine();
-                        if(isServerRunning){
-                                                prrintStream.println(Constants.SERVER_RUNNING);
-                                                bufferedReader.readLine();
-                                                bufferedReader.readLine();
-                                                
-                                                                        prrintStream.println(userName);
-
-                                                                       prrintStream.println(Constants.PLAY_WITH_USER_RESPONSE);
-                                                                       
-                                                                             if (userResponse.equals(Constants.USER_ACCEPTED)) {
-                            prrintStream.println(Constants.USER_ACCEPTED);
-                            
-                            System.out.println("User Accepted");
-                            
-                        } else {
-                            prrintStream.println(Constants.USER_REJECTED);
-                            
-                            System.out.println("User Accepted");
-                            
-                        }
-
-
-                        }else{
-                        
-                  prrintStream.println(Constants.SERVER_STOP);
-                        }                   
-                  
+                    } else {
+                        System.out.println("null type");
                     }
-                }
-                 
-                 
+
                 } else {
-                    // Server Stopped
                     prrintStream.println(Constants.SERVER_STOP);
-}
+                }
             } catch (IOException ex) {
                 Logger.getLogger(ServerHandler.class.getName()).log(Level.SEVERE, null, ex);
             } catch (Exception ex) {
                 Logger.getLogger(ServerHandler.class.getName()).log(Level.SEVERE, null, ex);
-            } 
+            }
         }
     }
 
-    
-    
-    
-    
-            void loginChecker(){
-        try{
+    void loginChecker() throws Exception {
+        String userName = bufferedReader.readLine();
+        String password = bufferedReader.readLine();
+        prrintStream.println(userName); // Return UserName
+        prrintStream.println(Constants.LOGIN); // Return Log In
+        User user = new User(userName, password, server.models.State.ONLINE);
+        if (dataAccess.logIn(user)) {
+            prrintStream.println(Constants.VALID_LOGIN);
+            useNameG = userName;
+        } else {
+            prrintStream.println(Constants.NOT_VALID_LOGIN);
+        }
 
-                    String userName = bufferedReader.readLine();
-   String password = bufferedReader.readLine();
-   try {
-       prrintStream.println(userName); // Return UserName
-       prrintStream.println(Constants.LOGIN); // Return Log In
-       User user = new User(userName, password, server.models.State.ONLINE);
-       if (dataAccess.logIn(user)) {
-           // Log In Complete
-           prrintStream.println(Constants.VALID_LOGIN);
-           // Send the connected user to UsersBase
-       } else {
-           // Wrong UserName or Password
-           prrintStream.println(Constants.NOT_VALID_LOGIN);
-       }
-   } catch (Exception ex) {
-       Logger.getLogger(ServerHandler.class.getName()).log(Level.SEVERE, null, ex);
-   }
+    }
 
-        } catch (IOException ex) {
-Logger.getLogger(ServerHandler.class.getName()).log(Level.SEVERE, null, ex);
-} 
+    void RegisterChecker() throws Exception {
+
+        String userName = bufferedReader.readLine();
+        String password = bufferedReader.readLine();
+
+        prrintStream.println(userName);
+        prrintStream.println(Constants.REGISTER);
+        User user = new User(userName, password, server.models.State.ONLINE);
+        if (dataAccess.signUp(user)) {
+            prrintStream.println(Constants.VALID_REGISTER);
+            useNameG = userName;
+        } else {
+            prrintStream.println(Constants.NOT_VALID_REGISTER);
+        }
+
+    }
+
+    void getUsers() throws Exception {
+
+        // Client wants to get all users
+        String userName1 = bufferedReader.readLine();
+        prrintStream.println(userName1); // Return UserName
+        prrintStream.println(Constants.GET_USERS); // Return GetUsers
+
+        for (ServerHandler object : UsersBase.vector) {
+            System.out.println(object.useNameG);
+        }
+
+        // Return Users
+        ArrayList<String> users = dataAccess.getOnlineUsers();
+        int noOfUsers = users.size();
+        prrintStream.println(noOfUsers);
+        for (int i = 0; i < noOfUsers; i++) {
+            prrintStream.println(users.get(i));
+        }
+
+    }
+
+    void playWithOther() throws IOException {
+
+        userName = bufferedReader.readLine();
+        opUserName = bufferedReader.readLine();
+
+        for (ServerHandler object : UsersBase.vector) {
+
+            if (object.useNameG.equals(opUserName)) {
+                object.prrintStream.println(userName);
+                result = object.bufferedReader.readLine();
+                System.out.println(result);
+            }
+
+            if (object.useNameG.equals(userName)) {
+                object.prrintStream.println(result);
+            }
 
         }
-          void RegisterChecker(){
-        try{
-
-            // Sign up
-   String userName = bufferedReader.readLine();
-   String password = bufferedReader.readLine();
-   try {
-       prrintStream.println(userName); // Return UserName
-       prrintStream.println(Constants.REGISTER); // Return Sign Up
-       User user = new User(userName, password, server.models.State.ONLINE);
-       if (dataAccess.signUp(user)) {
-           // Sign Up Complete
-           prrintStream.println(Constants.VALID_REGISTER);
-           // Send the connected user to UsersBase
-       } else {
-           // Sign Up Failed
-           prrintStream.println(Constants.NOT_VALID_REGISTER);
-       }
-   } catch (SQLException ex) {
-       Logger.getLogger(ServerHandler.class.getName()).log(Level.SEVERE, null, ex);
-   } catch (Exception ex) {
-       Logger.getLogger(ServerHandler.class.getName()).log(Level.SEVERE, null, ex);
-   }
-
-        } catch (IOException ex) {
-Logger.getLogger(ServerHandler.class.getName()).log(Level.SEVERE, null, ex);
-} 
-
-        }
-          void getUsers(){
-        try{
-
-   // Client wants to get all users
-   String userName1 = bufferedReader.readLine();
-   prrintStream.println(userName1); // Return UserName
-   prrintStream.println(Constants.GET_USERS); // Return GetUsers
-
-   // Return Users
-   ArrayList<String> users = dataAccess.getOnlineUsers();
-   int noOfUsers = users.size();
-   prrintStream.println(noOfUsers);
-   for (int i = 0; i < noOfUsers; i++) {
-       prrintStream.println(users.get(i));
-   }
-
-        } catch (IOException ex) {
-Logger.getLogger(ServerHandler.class.getName()).log(Level.SEVERE, null, ex);
-} catch (Exception ex) { 
-Logger.getLogger(ServerHandler.class.getName()).log(Level.SEVERE, null, ex);
-} 
-
-        }
+    }
 }
--- src/server/utils/Constants.java
+++ src/server/utils/Constants.java
@@ -5,24 +5,37 @@ public class Constants {
 
 
     
-    public final static String HOST = "127.0.0.1";
-    public final static int PORT = 5008;
+public    final static String SERVER_STOP="SERVER_STOP";
+    public final static String SERVER_RUNNING = "SERVER_RUNNING";
+    public final static boolean OPEN_FOR_PLAY_WITH_OTHER = true;
 
+public final static String GET_USERS = "GET_USERS";
     
-      public    final static String SERVER_STOP="SERVER_STOP";
-         public final static String SERVER_RUNNING = "SERVER_RUNNING";
-
-    public    final static String LOGIN="LOGIN";
-            public final static String VALID_LOGIN = "VALID_LOGIN";
+                  public final static String THAT_ISNOT_MER = "ThatIsNotMeR";
+            public final static String THAT_IS_MER = "ThatIsMeR";
+public    final static String LOGIN="LOGIN";
+    public final static String VALID_LOGIN = "VALID_LOGIN";
     public    final static String NOT_VALID_LOGIN="NOT_VALID_LOGIN";
-    
+    public    final static String WAIT="W";
+
+    public final static String SEND_REQUEST_TO_PLAY = "SEND_REQUEST_TO_PLAY";
+    public final static String RECEIVE_RESPONSE_FROM_USER = "SEND_REQUEST_TO_PLAY";
+      public final static String USER_ACCEPTED = "USER_ACCEPTED";
+    public final static String USER_REJECTED = "USER_REJECTED";
+        public final static String RECEIVE_REQUEST_FROM_SERVER = "RECEIVE_REQUEST_FROM_SERVER";
+        public final static String NoThingArrive = "NoThingArrive";
+
+
+    public final static String PLAY_WITH_USER_RESPONSE = "PLAY_WITH_USER_RESPONSE";
+      public final static String THAT_ISNOT_ME = "ThatIsNotMe";
+            public final static String THAT_IS_ME = "ThatIsMe";
+  
+
     public    final static String REGISTER="REGISTER";
     public    final static String NOT_VALID_REGISTER="NOT_VALID_REGISTER";
     public    final static String VALID_REGISTER="VALID_REGISTER";
-    
-    public final static String GET_USERS = "GET_USERS";
-    public final static String SEBD_REQUEST_TO_PLAY = "SEBD_REQUEST_TO_PLAY";
-    public final static String PLAY_WITH_USER_RESPONSE = "PLAY_WITH_USER_RESPONSE";
-    public final static String USER_ACCEPTED = "USER_ACCEPTED";
-    public final static String USER_REJECTED = "USER_REJECTED";
+    public    final static String HOST="127.0.0.1";
+    public    final static int PORT=5008;
+
+
 }
